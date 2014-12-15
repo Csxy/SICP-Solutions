cc97d32012c98508edabe136a6ba0c8c7c45d8a7
Csxy <472187343@qq.com>
2014-12-16 06:44:37 +0800

    4.3.2

---
+++ Chapter4/4.44.rkt
@@ -0,0 +1,21 @@
+#lang planet neil/sicp
+
+(define (queens board-size)
+  (define (queen-cols k positions)
+    (if (= k board-size)
+        positions
+        (let ((new (an-integer-between 1 board-size)))
+          (require (safe? new positions))
+          (queen-cols (+ k 1) (cons new positions)))))
+  (queen-cols 0 '()))
+
+(define (safe? k solution)
+  (define (attack? row col)
+    (or (= k row)
+        (= k (+ row col))
+        (= k (- row col))))
+  (define (check rest i)
+    (cond ((null? rest) #t) 
+          ((conflict? (car rest) i) #f) 
+          (else (check (cdr rest) (+ i 1)))))
+  (check solution 1))
