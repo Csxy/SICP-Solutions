6836a1e3652898b69a8fe1a3a9ad944f93975f31
Csxy <472187343@qq.com>
2014-12-18 00:22:06 +0800

    4.3.2

--- Chapter4/4.49.rkt
+++ Chapter4/4.49.rkt
@@ -1,7 +1,12 @@
 #lang planet neil/sicp
 
+(define (list-ramb alist) 
+   (if (null? alist) 
+       (ramb) 
+       (ramb (car alist) (list-ramb (cdr alist))))) 
+
 (define (parse-word word-list)
   (require (not (null? *unparsed*)))
   (require (memq (car *unparsed*) (cdr word-list)))
   (set! *unparsed* (cdr *unparsed*))
-  (apply amb (cdr word-list)))
+  (list-ramb (cdr word-list)))
